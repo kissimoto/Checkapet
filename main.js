730c44f15fc01b1a91a30a50d71dea7530ae6f7b
kissimoto <bmc_hit@yahoo.com>
2025-05-27 14:02:46 +0100

    Update comment out code to test main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 // Main JavaScript for CheckAPet website
 document.addEventListener('DOMContentLoaded', function() {
     // Mobile navigation toggle enhancement
@@ -40,3 +41,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+*/
